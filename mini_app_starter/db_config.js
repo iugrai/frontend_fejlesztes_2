7f00af949c88b01f6310f24064f3c4547812589d
Istvan Ugrai <iugrai@yahoo.com>
2021-05-29 15:09:53 +0200

    mini app starter órai feladat kész

---
+++ mini_app_starter/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyANKjhMBvd-ra0CcAtT3chTVMkctl3fZ7s',
+  authDomain: 'chat-app-istvan.firebaseapp.com',
+  databaseURL: 'https://chat-app-istvan.firebaseio.com',
+  projectId: 'chat-app-istvan',
+  storageBucket: 'chat-app-istvan.appspot.com',
+  messagingSenderId: '630781627986'
+};
+
+export default config;
